f0f646012c2c6cfd0a42f6f7e0b701112aed998a
Ezzat Ahmed <38844471+EzzatAhmed@users.noreply.github.com>
2023-03-30 20:05:30 +0200

    add comments in program.cs

--- CRUDOperationsTask/Program.cs
+++ CRUDOperationsTask/Program.cs
@@ -6,20 +6,33 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add Connection String From AppSettings
 var conString = builder.Configuration.GetConnectionString("Company");
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add DBContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(conString));
+
+//Add Services For Injections
 builder.Services.ServiceLibrary();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// add swagger for production 
+else if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
